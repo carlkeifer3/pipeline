7c73a7d6016eb5f7233ab9f0bd39365582a94e3b
carl keifer <carlkeifer3@gmail.com>
2015-01-15 08:08:00 -0500

    loading again. should be ready to add icons

--- Maya/pipeline/general/filebrowse/thumbview.py
+++ Maya/pipeline/general/filebrowse/thumbview.py
@@ -23,3 +23,52 @@ class thumbView(QtGui.QListView):
         self.tview.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
 
         self.thumbViewModel = QtGui.QStandardItemModel(self.tview)
+        #self.thumbViewModel.setSortRole()
+        self.tview.setModel(self.thumbViewModel)
+
+        self.thumbsDir = QtCore.QDir()
+        self.fileFilters = QtCore.QStringList
+
+
+    def setThumbColors(self):
+        self.bgColor = QtCore.QString("background: rgb(%1, %2, %3)")
+
+
+
+
+    def selectCurrentIndex(self):
+
+        if self.currentIndex.isValid() && self.thumbViewModel.rowCount() > 0:
+            self.tview.scrollTo(self.currentIndex)
+            self.tview.setCurrentIndex(self.currentIndex)
+
+    def getSingleSelectionFilename(self):
+        if self.tview.selectionModel().selectedIndexes().size() == 1:
+            return self.thumbViewModel.item(self.tview.selectionModel())
+        return("")
+
+    def getNextRow(self):
+        if self.currentRow == self.thumbViewModel.rowCount() -1:
+            return -1
+        return self.currentRow + 1
+
+    def getPrevRow(self):
+        if self.currentRow == 0:
+            return -1
+        return self.currentRow -1
+
+
+
+
+    def initThumbs(self):
+
+        self.thumbFileInfoList = self.thumbsDir.entryInfoList()
+
+
+        for currThumb = 0; currThumb < self.thumbFileInfoList.size; ++currThumb:
+            thumbFileInfo = self.thumbFileInfoList.at(currThumb)
+            thumbIitem = QtGui.QStandardItem()
+            thumbIitem.setData(self.thumbFileInfo.filePath(), FileNameRole)
+
+            self.thumbViewModel.appendRow(thumbIitem)
+
