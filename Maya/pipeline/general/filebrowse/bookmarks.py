1780b841e8d0c165d13394caeeaf03ddbb4b5cbd
carl keifer <carlkeifer3@gmail.com>
2015-02-09 15:48:24 -0500

    filing in Material from the update I wrote in the previous week.

--- Maya/pipeline/general/filebrowse/bookmarks.py
+++ Maya/pipeline/general/filebrowse/bookmarks.py
@@ -3,14 +3,25 @@
 __author__ = 'cargoyle'
 
 """
+import logging
 from PyQt4 import QtGui, QtCore
+import pipeline.general.filebrowse.GData as g
 
 class BookMarks(QtGui.QTreeWidget):
 
     def __init__(self, parent=None):
         import os
+        import pymel.core as pm
 
         # initialize the QlistView
         QtGui.QTreeWidget.__init__(self, parent)
 
+        self.GData = g.GData()
+
+        #locate the directory where all of the images for the UI live
+        ScriptDir = pm.internalVar(uad = True)
+        self.imgDirectory = str(ScriptDir+"/scripts/pipeline/general/filebrowse/images/")
+
         self.setAcceptDrops(True)
+        self.setDragEnabled(False)
+        self.setDragDropMode(QtGui.QAbstractItemView.DropOnly)
